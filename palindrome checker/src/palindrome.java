7899daa73ff880f21a2bba556d8f3343400d667b
lakshmi-sree3 <lakshmisreepathikapalu@gmail.com>
2026-03-11 16:08:23 +0530

    UC2: Print a Hardcoded Palindrome Result

--- palindrome checker/src/palindrome.java
+++ palindrome checker/src/palindrome.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+public class palindrome{
+    static void main() {
+                String original = "radar";
 
-public class palindrome {
-}
+                // Reverse the string
+                String reversed = new StringBuilder(original).reverse().toString();
+
+                // Check if original equals reversed
+                if (original.equals(reversed)) {
+                    System.out.println(original + " is a palindrome.");
+                } else {
+                    System.out.println(original + " is not a palindrome.");
+                }
+            }
+        }
+
+
